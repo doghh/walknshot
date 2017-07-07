8df23c61a60eb88f106fecb9780be6f888a26178
wsh <649489943@qq.com>
2017-07-07 16:04:07 +0800

    android: main activity finished

--- code/androidclient/app/src/main/java/cn/edu/sjtu/se/walknshot/androidclient/PersonalCenterFragment.java
+++ code/androidclient/app/src/main/java/cn/edu/sjtu/se/walknshot/androidclient/PersonalCenterFragment.java
@@ -1,17 +1,35 @@
 package cn.edu.sjtu.se.walknshot.androidclient;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class PersonalCenterFragment extends Fragment {
+
+    private ImageView mBtnGoSetting;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal_center, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+        mBtnGoSetting = view.findViewById(R.id.persenal_center_btn_setting);
+
+        mBtnGoSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SettingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ code/androidclient/app/src/main/java/cn/edu/sjtu/se/walknshot/androidclient/SettingActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.sjtu.se.walknshot.androidclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends MyAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
